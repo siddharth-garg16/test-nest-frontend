cc603c4352a4029fbd8db20fa1173fb149f215f8
Siddharth Garg <siddharthgarg16082000@gmail.com>
2026-03-21 01:59:57 +0530

    change: removed material and added tailwind for ui enhancement

--- src/app/core/components/login/login.ts
+++ src/app/core/components/login/login.ts
@@ -1,23 +1,9 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [
-    MatCardModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
-    MatDividerModule,
-    RouterLink,
-  ],
+  imports: [RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/core/components/sign-up/sign-up.ts
+++ src/app/core/components/sign-up/sign-up.ts
@@ -1,25 +1,9 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [
-    MatCardModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
-    MatDividerModule,
-    RouterLink,
-    MatRadioModule,
-  ],
+  imports: [RouterLink],
   templateUrl: './sign-up.html',
   styleUrl: './sign-up.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
