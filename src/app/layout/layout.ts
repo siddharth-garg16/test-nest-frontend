193dd46d71482c16b978bde1527b66711402c426
Siddharth Garg <siddharthgarg16082000@gmail.com>
2026-03-28 15:36:18 +0530

    add: layout set app for authenticated user

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Navbar } from './navbar/navbar';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Navbar],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
